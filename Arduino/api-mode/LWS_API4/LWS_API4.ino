2509d96abeea8029060a90bfe5b2bbf6b609778a
Shyla Clark <shylaclark@gmail.com>
2018-02-27 14:15:57 -0600

    Moved to CLion

--- Arduino/api-mode/LWS_API4/LWS_API4.ino
+++ Arduino/api-mode/LWS_API4/LWS_API4.ino
@@ -11,21 +11,33 @@ void setup() {
   xbee.setSerial(Serial);     
 }
 
-// Specify the address of the remote XBee (this is the SH + SL)
-// Special value 0x0000000000000000 can be used to address coordinator
-XBeeAddress64 addr64 = XBeeAddress64(0x00000000, 0x00000000);
 
-int data = analogRead(A0); 
-uint8_t* payload = (uint8_t*) data;
 
 //uint8_t* payload = analogRead(A0);
 
-// ZB Transmit Request API frame containing dest. address + message
-ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
+
+
+void sendPacket() {
+
+  // Specify the address of the remote XBee (this is the SH + SL)
+  // Special value 0x0000000000000000 can be used to address coordinator
+  XBeeAddress64 addr64 = XBeeAddress64(0x00000000, 0x00000000);
+
+  int data = analogRead(A0); 
+  uint8_t* payload = (uint8_t*) data;
+  
+  // ZB Transmit Request API frame/packet containing dest. address + message
+  ZBTxRequest txPacket = ZBTxRequest(addr64, payload, sizeof(payload));
+  xbee.send(txPacket);
+
+}
+
+unsigned long last_tx_time = 0;
 
 void loop() {
   // Send your request
-  xbee.send(zbTx);
   delay(2000);
   Serial.println("Sent");
 }
+
+
