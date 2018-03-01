7991bcc5e6b3c1b47b12e91961e09f081edb08e4
Shyla Clark <shylaclark@gmail.com>
2018-03-01 13:54:59 -0600

    Removed old py files

--- Py/testing/HelloReceive.py
+++ Py/testing/HelloReceive.py
@@ -1,38 +1,25 @@
-#! /usr/bin/python.3.5.1
+# /usr/bin/python.3.5.3
 # Check for new /dev/ttyUSB* before running
 # XBee radios in API mode - not transparent mode
-# Threaded synchronous mode 
+# Threaded asynchronous mode 
 
-
-import serial
-import time
 from xbee import ZigBee
+import serial
 
 # Open serial port on Pi
 # serial_port = serial.Serial('/dev/ttyUSB0', 9600)
+
 # Open serial port on Mac
 serial_port = serial.Serial('/dev/tty.usbserial-DN03FEEK', 9600)
 
-def message_received(data):
-    """
-    This method is called whenever data is received
-    from the associated XBee device. Its first and
-    only argument is the data contained within the
-    frame.
-    """
-    print (data)
+xbee = ZigBee(serial_port)
 
-# Create API object, which spawns a new thread
-xbee = ZigBee(serial_port, callback=message_received)
 
 # Do other stuff in the main thread
 while True:
     try:
-        print ("Where is the message?")
-        time.sleep(1)
+        print (xbee.wait_read_frame())
     except KeyboardInterrupt:
         break
 
-# Always call halt() before closing serial for proper thread shutdown
-xbee.halt()
 serial_port.close()
